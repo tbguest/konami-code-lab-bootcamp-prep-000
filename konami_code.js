f95b3676f4c6a0974f2b8e32394e1c9506da7ad1
Tristan Guest <tristanguest@gmail.com>
2020-06-19 14:46:25 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -35,25 +35,4 @@ function init() {
 }
 
 
-
-// let index = 0;
- 
-// // This is the function that would be invoked by the event listener.
-// function onKeyDownHandler(e) {
-//   const key = e.key;
- 
-//   if (key === codes[index]) {
-//     index++;
- 
-//     if (index === codes.length) {
-//       alert("Cheater!");
- 
-//       index = 0;
-//     }
-//   } else {
-//     index = 0;
-//   }
-// }
-
-
 // init()
